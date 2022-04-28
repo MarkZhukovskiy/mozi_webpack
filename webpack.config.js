a5453c7549e74ed25830ecec846d6318013eba8d
Марк Жуковский <markzukovskij@MacBook-Pro-Mark.local>
2022-04-28 14:43:50 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const htmlWebpack = require('html-webpack-plugin')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const FileIncludeWebpackPlugin = require('file-include-webpack-plugin')
+
+
+ 
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: ['./src/js/index.js', './src/styles/component.js']
+    },
+    output: {
+        filename: './index.min.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [{
+            test: /\.js$/i,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+        },
+        // изображения
+        {
+            test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+            type: 'asset/resource',
+        },
+        // шрифты и SVG
+        {
+            test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+            type: 'asset/inline',
+        },
+        {
+            test: /\.(scss|css)$/,
+            use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+
+    ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new htmlWebpack({
+            template: './src/html/index.html',
+            filename: 'index.html',
+            inject: true,
+            minify: true
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 9000,
+            server: { baseDir: ['./dist'] }
+        }),
+        new FileIncludeWebpackPlugin(
+            {
+              source: './src/html'
+            }
+        )
+    ]
+}
